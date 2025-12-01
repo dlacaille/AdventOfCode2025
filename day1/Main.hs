96e02c5f1e0524e63461b145439a5ed084debb50
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-01 10:42:49 -0500

    Clean up code

--- day1/Main.hs
+++ day1/Main.hs
@@ -1,24 +1,24 @@
 module Main where
 
-import Data.Char (toUpper)
 import System.IO
 
-rotateDialLeft :: Int -> Int -> Int
-rotateDialLeft dial x = (dial - x) `mod` 100
-
-rotateDialRight :: Int -> Int -> Int
-rotateDialRight dial x = (dial + x) `mod` 100
-
 rotateDial :: Int -> Char -> Int -> Int
-rotateDial dial 'L' x = rotateDialLeft dial x
-rotateDial dial 'R' x = rotateDialRight dial x
+rotateDial dial 'L' x = (dial - x) `mod` 100
+rotateDial dial 'R' x = (dial + x) `mod` 100
+rotateDial dial _ _ = dial
 
 countZero :: Int -> Int -> Int
 countZero acc 0 = acc + 1
 countZero acc _ = acc
 
+countTimesPassedZero :: Int -> Char -> Int -> Int
+countTimesPassedZero dial 'L' x = (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
+countTimesPassedZero dial 'R' x = (x + dial) `div` 100
+countTimesPassedZero _ _ _ = 0
+
 parseLine :: String -> (Char, Int)
-parseLine l = let (x:xs) = l in (toUpper x, read xs)
+parseLine [] = ('R', 0)
+parseLine l = let (x : xs) = l in (x, read xs)
 
 parseAndRotate :: Int -> String -> Int
 parseAndRotate dial l = let (dir, x) = parseLine l in rotateDial dial dir x
@@ -27,33 +27,24 @@ parseAndRotateAndCountZero :: (Int, Int) -> String -> (Int, Int)
 parseAndRotateAndCountZero (dial, acc) l =
     let newDial = parseAndRotate dial l
         newAcc = countZero acc newDial
-    in (newDial, newAcc)
+     in (newDial, newAcc)
 
 parseAndRotateAndCountPassedZero :: (Int, Int) -> String -> (Int, Int)
 parseAndRotateAndCountPassedZero (dial, acc) l =
     let (dir, x) = parseLine l
-        timesPassedZero = case dir of
-            'L' -> (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
-            'R' -> (x + dial) `div` 100
         newDial = rotateDial dial dir x
-        newAcc = acc + timesPassedZero
-    in (newDial, newAcc)
+        newAcc = acc + countTimesPassedZero dial dir x
+     in (newDial, newAcc)
 
-puzzle1 :: IO ()
-puzzle1 = do
-    handle <- openFile "input" ReadMode
-    contents <- hGetContents handle
-    print $ snd $ foldl parseAndRotateAndCountZero (50, 0) (lines contents)
-    
-puzzle2 :: IO ()
-puzzle2 = do
-    handle <- openFile "input" ReadMode
-    contents <- hGetContents handle
-    print $ snd $ foldl parseAndRotateAndCountPassedZero (50, 0) (lines contents)
-    
+puzzle1 :: [String] -> Int
+puzzle1 l = snd $ foldl parseAndRotateAndCountZero (50, 0) l
+
+puzzle2 :: [String] -> Int
+puzzle2 l = snd $ foldl parseAndRotateAndCountPassedZero (50, 0) l
+
 main :: IO ()
 main = do
-    putStr "Puzzle 1: "
-    puzzle1
-    putStr "Puzzle 2: "
-    puzzle2
+    handle <- openFile "input" ReadMode
+    contents <- hGetContents handle
+    putStr $ "Puzzle 1: " ++ show (puzzle1 $ lines contents)
+    putStr $ "Puzzle 2: " ++ show (puzzle2 $ lines contents)
