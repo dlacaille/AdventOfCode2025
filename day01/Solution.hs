ce8593a2de1ba135ebe528191a1635a8c1702c1e
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-01 16:50:13 -0500

    Use cleaner case syntax

--- day01/Solution.hs
+++ day01/Solution.hs
@@ -5,14 +5,16 @@ parseLine [] = ('R', 0)
 parseLine l = let (x : xs) = l in (x, read xs)
 
 rotateDial :: Int -> Char -> Int -> Int
-rotateDial dial 'L' x = (dial - x) `mod` 100
-rotateDial dial 'R' x = (dial + x) `mod` 100
-rotateDial dial _ _ = dial
+rotateDial dial dir x = case dir of
+  'L' -> (dial - x) `mod` 100
+  'R' -> (dial + x) `mod` 100
+  _ -> dial
 
 countTimesPassedZero :: Int -> Char -> Int -> Int
-countTimesPassedZero dial 'L' x = (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
-countTimesPassedZero dial 'R' x = (x + dial) `div` 100
-countTimesPassedZero _ _ _ = 0
+countTimesPassedZero dial dir x = case dir of
+  'L' -> (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
+  'R' -> (x + dial) `div` 100
+  _ -> 0
 
 puzzle1 :: [String] -> Int
 puzzle1 = snd . foldl rotate (50, 0) . map parseLine
