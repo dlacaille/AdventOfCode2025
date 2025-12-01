c8d7436ed586a252a4227bdc886d895c4b92c93f
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-01 16:55:33 -0500

    Use haskell data type

--- day01/Solution.hs
+++ day01/Solution.hs
@@ -1,20 +1,23 @@
 module Solution where
 
-parseLine :: String -> (Char, Int)
-parseLine [] = ('R', 0)
-parseLine l = let (x : xs) = l in (x, read xs)
+data Dir = L | R deriving (Show, Eq)
 
-rotateDial :: Int -> Char -> Int -> Int
-rotateDial dial dir x = case dir of
-  'L' -> (dial - x) `mod` 100
-  'R' -> (dial + x) `mod` 100
-  _ -> dial
+toDir :: Char -> Dir
+toDir 'L' = L
+toDir 'R' = R
+toDir _ = R
 
-countTimesPassedZero :: Int -> Char -> Int -> Int
-countTimesPassedZero dial dir x = case dir of
-  'L' -> (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
-  'R' -> (x + dial) `div` 100
-  _ -> 0
+parseLine :: String -> (Dir, Int)
+parseLine [] = (R, 0)
+parseLine l = let (x : xs) = l in (toDir x, read xs)
+
+rotateDial :: Int -> Dir -> Int -> Int
+rotateDial dial L x = (dial - x) `mod` 100
+rotateDial dial R x = (dial + x) `mod` 100
+
+countTimesPassedZero :: Int -> Dir -> Int -> Int
+countTimesPassedZero dial L x = (x - dial) `div` 100 + (if dial > 0 then 1 else 0)
+countTimesPassedZero dial R x = (x + dial) `div` 100
 
 puzzle1 :: [String] -> Int
 puzzle1 = snd . foldl rotate (50, 0) . map parseLine
