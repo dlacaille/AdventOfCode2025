3e1fc85952ef6d9123298c368de160316c502a53
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-02 16:43:48 -0500

    More clean up

--- day02/Solution.hs
+++ day02/Solution.hs
@@ -11,23 +11,20 @@ isRepeating :: (Eq a) => Int -> [a] -> Bool
 isRepeating n = allEqual . chunksOf n
 
 isRepeatedTwice :: (Eq a) => [a] -> Bool
-isRepeatedTwice s
-  | odd (length s) = False
-  | otherwise = isRepeating (length s `div` 2) s
+isRepeatedTwice xs = even len && isRepeating (len `div` 2) xs
+ where
+  len = length xs
 
 isPeriodic :: (Eq a) => [a] -> Bool
-isPeriodic s = any (`isRepeating` s) $ (init . divisors) (length s)
+isPeriodic s = any (`isRepeating` s) . init . divisors $ length s
 
 toRange :: String -> (Int, Int)
-toRange s =
-  case splitOn "-" s of
-    [start, end] -> (read start, read end)
-    _ -> (0, 0)
+toRange s = case splitOn "-" s of
+  [start, end] -> (read start, read end)
+  _ -> (0, 0)
 
-puzzle1 :: [(Int, Int)] -> Int
+puzzle1, puzzle2 :: [(Int, Int)] -> Int
 puzzle1 =
   sum . map read . concatMap (filter isRepeatedTwice . map show . range)
-
-puzzle2 :: [(Int, Int)] -> Int
 puzzle2 =
   sum . map read . concatMap (filter isPeriodic . map show . range)
