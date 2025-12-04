7f09f1efcc099bbd2a489224d385fb3fdada937f
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-04 15:28:13 -0500

    Improve performance drastically with vector

--- day04/Solution.hs
+++ day04/Solution.hs
@@ -1,58 +1,63 @@
 module Solution where
 
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import Data.Maybe (mapMaybe)
+import Data.Vector (Vector, (!?))
+import qualified Data.Vector as V
 
-type CharGrid = IntMap (IntMap Char)
+type CharGrid = Vector (Vector Char)
 
 charAt :: Int -> Int -> CharGrid -> Maybe Char
-charAt x y input = IntMap.lookup y input >>= IntMap.lookup x
+charAt x y grid = grid !? y >>= (!? x)
 
 gridFromLines :: [String] -> CharGrid
-gridFromLines xs =
-  IntMap.fromList
-    [ (y, IntMap.fromList [(x, c) | (x, c) <- zip [0 ..] line])
-    | (y, line) <- zip [0 ..] xs
-    ]
+gridFromLines = V.fromList . map V.fromList
 
-adjacentChars :: Int -> Int -> CharGrid -> [Char]
-adjacentChars x y input =
-  let aboveRow = [(x - 1, y - 1), (x, y - 1), (x + 1, y - 1)]
-      targetRow = [(x - 1, y), (x + 1, y)]
-      belowRow = [(x - 1, y + 1), (x, y + 1), (x + 1, y + 1)]
-      positions = aboveRow ++ targetRow ++ belowRow
-   in [c | (row, col) <- positions, Just c <- [charAt row col input]]
+adjacentPositions :: Int -> Int -> [(Int, Int)]
+adjacentPositions x y =
+  [ (x + dx, y + dy)
+  | dy <- [-1, 0, 1]
+  , dx <- [-1, 0, 1]
+  , (dx, dy) /= (0, 0)
+  ]
+
+adjacentChars :: Int -> Int -> CharGrid -> String
+adjacentChars x y grid = mapMaybe (\(px, py) -> charAt px py grid) (adjacentPositions x y)
 
 countMatchingAdjacent :: Int -> Int -> CharGrid -> Int
-countMatchingAdjacent x y input =
-  length $ filter (\a -> Just a == charAt x y input) $ adjacentChars x y input
+countMatchingAdjacent x y grid =
+  length $ filter (== charAt x y grid) $ map Just $ adjacentChars x y grid
 
-gridSize :: IntMap (IntMap a) -> (Int, Int)
-gridSize input =
-  let width = length (head $ IntMap.elems input)
-      height = length input
-   in (width, height)
+gridSize :: CharGrid -> (Int, Int)
+gridSize grid
+  | V.null grid = (0, 0)
+  | otherwise = (V.length (V.head grid), V.length grid)
 
 positionsOfChar :: Char -> CharGrid -> [(Int, Int)]
-positionsOfChar char input =
-  let (width, height) = gridSize input
-   in [ (x, y)
-      | y <- [0 .. height - 1]
-      , x <- [0 .. width - 1]
-      , charAt x y input == Just char
-      ]
+positionsOfChar char grid =
+  [ (x, y)
+  | y <- [0 .. height - 1]
+  , x <- [0 .. width - 1]
+  , charAt x y grid == Just char
+  ]
+ where
+  (width, height) = gridSize grid
 
 replaceCharAt :: Int -> Int -> Char -> CharGrid -> CharGrid
-replaceCharAt x y newChar =
-  IntMap.adjust (IntMap.insert x newChar) y
+replaceCharAt x y newChar grid = grid V.// [(y, row V.// [(x, newChar)])]
+ where
+  row = grid V.! y
 
-puzzle1, puzzle2 :: Char -> CharGrid -> IO Int
-puzzle1 c input = return $ length $ filter (< 4) $ map (\(x, y) -> countMatchingAdjacent x y input) $ positionsOfChar c input
-puzzle2 c input = do
-  let positions = filter ((< 4) . \(x, y) -> countMatchingAdjacent x y input) $ positionsOfChar c input
-  if null positions
-    then return 0
-    else do
-      let newInput = foldl (\i (x, y) -> replaceCharAt x y '.' i) input positions
-      x <- puzzle2 c newInput
-      return $ length positions + x
+puzzle1, puzzle2 :: Char -> [String] -> Int
+puzzle1 c input = length $ filter (< 4) $ map (uncurry countMatchingAdjacent') positions
+ where
+  grid = gridFromLines input
+  positions = positionsOfChar c grid
+  countMatchingAdjacent' x y = countMatchingAdjacent x y grid
+puzzle2 c input = go (gridFromLines input)
+ where
+  go grid
+    | null positions = 0
+    | otherwise = length positions + go newGrid
+   where
+    positions = filter (\(x, y) -> countMatchingAdjacent x y grid < 4) $ positionsOfChar c grid
+    newGrid = foldr (\(x, y) g -> replaceCharAt x y '.' g) grid positions
--- day04/test/Spec.hs
+++ day04/test/Spec.hs
@@ -7,7 +7,7 @@ main :: IO ()
 main = hspec $ do
   describe "adjacentChars" $ do
     it "returns adjacent characters from a grid" $ do
-      let input =
+      let grid =
             gridFromLines
               [ "12345"
               , "67890"
@@ -15,13 +15,13 @@ main = hspec $ do
               , "fghij"
               , "klmno"
               ]
-      adjacentChars 2 2 input `shouldBe` "789bdghi"
-      adjacentChars 0 0 input `shouldBe` "267"
-      adjacentChars 4 4 input `shouldBe` "ijn"
+      adjacentChars 2 2 grid `shouldBe` "789bdghi"
+      adjacentChars 0 0 grid `shouldBe` "267"
+      adjacentChars 4 4 grid `shouldBe` "ijn"
 
   describe "countMatchingAdjacent" $ do
     it "counts adjacent characters matching the target character" $ do
-      let input =
+      let grid =
             gridFromLines
               [ "..xx.xx@x."
               , "x@@.@.@.@@"
@@ -34,79 +34,77 @@ main = hspec $ do
               , ".@@@@@@@@."
               , "x.x.@@@.x."
               ]
-      countMatchingAdjacent 0 0 input `shouldBe` 1
-      countMatchingAdjacent 3 3 input `shouldBe` 7
+      countMatchingAdjacent 0 0 grid `shouldBe` 1
+      countMatchingAdjacent 3 3 grid `shouldBe` 7
 
   describe "gridSize" $ do
     it "returns the size of the grid as (width, height)" $ do
-      let input =
+      let grid =
             gridFromLines
               [ "abcd"
               , "efgh"
               , "ijkl"
               ]
-      gridSize input `shouldBe` (4, 3)
+      gridSize grid `shouldBe` (4, 3)
 
   describe "positionsOfChar" $ do
     it "finds all positions of a given character in the grid" $ do
-      let input =
+      let grid =
             gridFromLines
               [ "abca"
               , "defa"
               , "ghia"
               ]
-      positionsOfChar 'a' input `shouldBe` [(0, 0), (3, 0), (3, 1), (3, 2)]
-      positionsOfChar 'd' input `shouldBe` [(0, 1)]
+      positionsOfChar 'a' grid `shouldBe` [(0, 0), (3, 0), (3, 1), (3, 2)]
+      positionsOfChar 'd' grid `shouldBe` [(0, 1)]
 
   describe "replaceCharAt" $ do
     it "replaces a character at a specific position in the grid" $ do
-      let input =
+      let grid =
             gridFromLines
               [ "abcd"
               , "efgh"
               , "ijkl"
               ]
-      replaceCharAt 1 1 '.' input `shouldBe` gridFromLines ["abcd", "e.gh", "ijkl"]
-      replaceCharAt 0 0 '.' input `shouldBe` gridFromLines [".bcd", "efgh", "ijkl"]
+      charAt 1 1 (replaceCharAt 1 1 '.' grid) `shouldBe` Just '.'
+      charAt 0 0 (replaceCharAt 0 0 '.' grid) `shouldBe` Just '.'
 
   describe "puzzle1" $ do
     it "solves the puzzle for given input" $ do
       let input =
-            gridFromLines
-              [ "..@@.@@@@."
-              , "@@@.@.@.@@"
-              , "@@@@@.@.@@"
-              , "@.@@@@..@."
-              , "@@.@@@@.@@"
-              , ".@@@@@@@.@"
-              , ".@.@.@.@@@"
-              , "@.@@@.@@@@"
-              , ".@@@@@@@@."
-              , "@.@.@@@.@."
-              ]
-      puzzle1 '@' input `shouldReturn` 13
+            [ "..@@.@@@@."
+            , "@@@.@.@.@@"
+            , "@@@@@.@.@@"
+            , "@.@@@@..@."
+            , "@@.@@@@.@@"
+            , ".@@@@@@@.@"
+            , ".@.@.@.@@@"
+            , "@.@@@.@@@@"
+            , ".@@@@@@@@."
+            , "@.@.@@@.@."
+            ]
+      puzzle1 '@' input `shouldBe` 13
     it "processes input file" $ do
       contents <- readFile "input"
-      result <- puzzle1 '@' (gridFromLines $ lines contents)
+      let result = puzzle1 '@' (lines contents)
       pendingWith $ show result
 
   describe "puzzle2" $ do
     it "solves the puzzle for given input" $ do
       let input =
-            gridFromLines
-              [ "..@@.@@@@."
-              , "@@@.@.@.@@"
-              , "@@@@@.@.@@"
-              , "@.@@@@..@."
-              , "@@.@@@@.@@"
-              , ".@@@@@@@.@"
-              , ".@.@.@.@@@"
-              , "@.@@@.@@@@"
-              , ".@@@@@@@@."
-              , "@.@.@@@.@."
-              ]
-      puzzle2 '@' input `shouldReturn` 43
+            [ "..@@.@@@@."
+            , "@@@.@.@.@@"
+            , "@@@@@.@.@@"
+            , "@.@@@@..@."
+            , "@@.@@@@.@@"
+            , ".@@@@@@@.@"
+            , ".@.@.@.@@@"
+            , "@.@@@.@@@@"
+            , ".@@@@@@@@."
+            , "@.@.@@@.@."
+            ]
+      puzzle2 '@' input `shouldBe` 43
     it "processes input file" $ do
       contents <- readFile "input"
-      result <- puzzle2 '@' (gridFromLines $ lines contents)
+      let result = puzzle2 '@' (lines contents)
       pendingWith $ show result
