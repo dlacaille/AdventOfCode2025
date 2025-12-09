6612a098635d547d5ab68a96581b2f64bdc0c129
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-09 13:23:41 -0500

    Found optimized version

--- day09/Solution.hs
+++ day09/Solution.hs
@@ -1,66 +1,32 @@
 module Solution where
 
-import Data.List (nub, sortOn)
+import Data.List (find, sortOn)
+import Data.Maybe (fromJust)
 import Data.Ord (Down (Down))
 import Utils
 
---- Get all points of a rectangle defined by two points
-rectPoints :: Vec2D -> Vec2D -> [Vec2D]
-rectPoints (x1, y1) (x2, y2) =
-  [ (x, y)
-  | x <- [min x1 x2 .. max x1 x2]
-  , y <- [min y1 y2 .. max y1 y2]
-  ]
-
---- Get all points on boundary and inside polygon (axis-aligned)
-polyPoints :: [(Int, Int)] -> [(Int, Int)]
-polyPoints poly = nub $ boundary ++ interior
- where
-  polyEdges = zip poly (tail poly ++ [head poly])
-  boundary = concatMap axisLine polyEdges
-  interior =
-    filter
-      (pointInPoly polyEdges)
-      [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY]]
-  (minX, maxX, minY, maxY) =
-    let (xs, ys) = unzip poly
-     in (minimum xs, maximum xs, minimum ys, maximum ys)
-
-  axisLine ((x0, y0), (x1, y1))
-    | x0 == x1 = [(x0, y) | y <- [min y0 y1 .. max y0 y1]]
-    | otherwise = [(x, y0) | x <- [min x0 x1 .. max x0 x1]]
-
-  pointInPoly edges (px, py) = odd $ length $ filter crossesRay edges
-   where
-    crossesRay ((x1, y1), (x2, y2))
-      | x1 /= x2 = False
-      | y1 <= py && y2 > py = px < x1
-      | y2 <= py && y1 > py = px < x1
-      | otherwise = False
-
---- Print all points of a polygon in a grid for debugging purposes
-printPoly :: [Vec2D] -> [Vec2D] -> [Vec2D] -> IO ()
-printPoly hes xes oes = mapM_ putStrLn rows
- where
-  rows =
-    [ [ renderCell x y
-      | x <- [0 .. maxX + 2]
-      ]
-    | y <- [0 .. maxY + 1]
-    ]
-  (maxX, maxY) =
-    let (xs, ys) = unzip xes
-     in (maximum xs, maximum ys)
-
-  renderCell x y
-    | (x, y) `elem` oes = 'O'
-    | (x, y) `elem` hes = '#'
-    | (x, y) `elem` xes = 'X'
-    | otherwise = '.'
-
---- Area of a rectangle between two points
+-- | Area of a rectangle between two points
 areaRect2D :: Vec2D -> Vec2D -> Int
 areaRect2D (x1, y1) (x2, y2) = (abs (x2 - x1) + 1) * (abs (y2 - y1) + 1)
+
+-- | Check if a rectangle doesn't cross any edges
+rectIsValid :: [(Vec2D, Vec2D)] -> [(Vec2D, Vec2D)] -> Vec2D -> Vec2D -> Bool
+rectIsValid hEdges vEdges (x1, y1) (x2, y2) =
+  let minX = min x1 x2
+      maxX = max x1 x2
+      minY = min y1 y2
+      maxY = max y1 y2
+      crossesHorizontal ((ex1, ey1), (ex2, _)) =
+        let edgeY = ey1
+            edgeMinX = min ex1 ex2
+            edgeMaxX = max ex1 ex2
+         in edgeY > minY && edgeY < maxY && edgeMinX < maxX && edgeMaxX > minX
+      crossesVertical ((ex1, ey1), (_, ey2)) =
+        let edgeX = ex1
+            edgeMinY = min ey1 ey2
+            edgeMaxY = max ey1 ey2
+         in edgeX > minX && edgeX < maxX && edgeMinY < maxY && edgeMaxY > minY
+   in not (any crossesHorizontal hEdges) && not (any crossesVertical vEdges)
 
 puzzle1 :: [Vec2D] -> IO Int
 puzzle1 xs = do
@@ -70,8 +36,9 @@ puzzle1 xs = do
 
 puzzle2 :: [Vec2D] -> IO Int
 puzzle2 xs = do
-  let poly = polyPoints xs
+  let edges = zip xs (tail xs ++ [head xs])
+      hEdges = [e | e@((_, y1), (_, y2)) <- edges, y1 == y2]
+      vEdges = [e | e@((x1, _), (x2, _)) <- edges, x1 == x2]
       areas = map (\(a, b) -> ((a, b), areaRect2D a b)) (allNonConsecPairs xs)
       sortedAreas = sortOn (Down . snd) areas
-      filteredAreas = filter (\((a, b), _) -> all (`elem` poly) (rectPoints a b)) sortedAreas
-  return $ snd $ head filteredAreas
+  return $ snd $ fromJust $ find (\((a, b), _) -> rectIsValid hEdges vEdges a b) sortedAreas
