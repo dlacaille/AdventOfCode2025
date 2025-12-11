9437de089345d4e1ba05c775214327a7380c2b6b
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-10 22:19:59 -0500

    Puzzle 2 brute-force-ish solution

--- day10/Solution.hs
+++ day10/Solution.hs
@@ -4,29 +4,43 @@ import Control.Monad (msum)
 import Data.List (sortOn)
 import Data.Maybe (listToMaybe, mapMaybe)
 
-type Indicator = Bool
 type Button = [Int]
-data Machine = Machine [Indicator] [Button] deriving (Show, Eq)
+type State a = [a]
+type NextButton a = Button -> [a] -> [a] -> [Button] -> [Button]
 
-toggleButton :: Button -> [Indicator] -> [Indicator]
-toggleButton button = zipWith (\i s -> if i `elem` button then not s else s) [0 ..]
+buttonsAfter :: NextButton Bool
+buttonsAfter button _ _ = filter (> button)
 
-solve :: [Button] -> [Indicator] -> [Indicator] -> [Button] -> [[Button]]
-solve pressedButtons initialState desiredState buttons
+checkState :: NextButton Int
+checkState _ state desiredState =
+  filter
+    ( \button ->
+        let newState = zipWith (\i s -> if i `elem` button then s + 1 else s) [0 ..] state
+         in and $ zipWith (>=) desiredState newState
+    )
+
+solve :: (Eq a, Show a) => [Button] -> (a -> a) -> NextButton a -> [a] -> [a] -> [Button] -> [[Button]]
+solve pressedButtons operation nextButtons initialState desiredState buttons
   | not (null solutions) = [button : pressedButtons | (_, button) <- take 1 solutions]
-  | otherwise = msum [solve (button : pressedButtons) state desiredState (filter (> button) buttons) | (state, button) <- nextStates]
+  | otherwise =
+      msum
+        [ let newPressed = (button : pressedButtons)
+              newButtons = nextButtons button state desiredState buttons
+           in solve newPressed operation nextButtons state desiredState newButtons
+        | (state, button) <- nextStates
+        ]
  where
-  nextStates = [(toggleButton btn initialState, btn) | btn <- buttons]
+  nextStates = [(zipWith (\i s -> if i `elem` btn then operation s else s) [0 ..] initialState, btn) | btn <- buttons]
   solutions = sortOn (length . snd) [(s, b) | (s, b) <- nextStates, s == desiredState]
 
-start :: Machine -> Maybe [Button]
-start (Machine indicators buttons) =
-  listToMaybe $ sortOn length $ solve [] initialState indicators (reverse buttons)
+start :: (Eq a, Show a) => ([a], [Button]) -> (a -> a) -> (Button -> [a] -> [a] -> [Button] -> [Button]) -> a -> Maybe [Button]
+start (desiredState, buttons) operation nextButtons initialValue =
+  listToMaybe $ sortOn length $ solve [] operation nextButtons initialState desiredState (reverse buttons)
  where
-  initialState = replicate (length indicators) False
+  initialState = replicate (length desiredState) initialValue
 
-puzzle1 :: [Machine] -> IO Int
-puzzle1 = return . sum . map length . mapMaybe start
+puzzle1 :: [([Bool], [Button])] -> IO Int
+puzzle1 = return . sum . map length . mapMaybe (\m -> start m not buttonsAfter False)
 
-puzzle2 :: IO Int
-puzzle2 = return 0
+puzzle2 :: [([Int], [Button])] -> IO Int
+puzzle2 = return . sum . map length . mapMaybe (\m -> start m (+ 1) checkState 0)
--- day10/test/Spec.hs
+++ day10/test/Spec.hs
@@ -6,88 +6,101 @@ import Test.Hspec
 
 main :: IO ()
 main = hspec $ do
-  describe "toggleButton" $ do
-    it "toggles the correct indicators" $ do
-      let button = [0, 2]
-          initialState = [False, False, False]
-      toggleButton button initialState `shouldBe` [True, False, True]
-    it "toggles multiple times" $ do
-      let button = [1, 2]
-          initialState = [True, True, False]
-      toggleButton button initialState `shouldBe` [True, False, True]
-
   describe "solve" $ do
     it "solves a simple problem" $ do
       let desiredState = [True]
           initialState = replicate (length desiredState) False
           buttons = [[0]]
-      solve [] initialState desiredState buttons `shouldBe` [[[0]]]
+      solve [] not buttonsAfter initialState desiredState buttons `shouldBe` [[[0]]]
     it "selects the right button" $ do
       let desiredState = [False, True]
           initialState = replicate (length desiredState) False
           buttons = [[0], [1]]
-      solve [] initialState desiredState buttons `shouldBe` [[[1]]]
+      solve [] not buttonsAfter initialState desiredState buttons `shouldBe` [[[1]]]
     it "selects multiple buttons" $ do
       let desiredState = [False, True, False]
           initialState = replicate (length desiredState) False
           buttons = [[2], [0, 1], [0]]
-      solve [] initialState desiredState buttons `shouldBe` [[[0, 1], [0]]]
+      solve [] not buttonsAfter initialState desiredState buttons `shouldBe` [[[0, 1], [0]]]
     it "uses initial state" $ do
       let desiredState = [False, True, False]
           initialState = [True, False, False]
           buttons = [[2], [0, 1]]
-      solve [] initialState desiredState buttons `shouldBe` [[[0, 1]]]
+      solve [] not buttonsAfter initialState desiredState buttons `shouldBe` [[[0, 1]]]
     it "uses already pressed buttons" $ do
       let desiredState = [False, True, True, False]
           initialState = [True, True, False, False]
           pressedButtons = [[0, 1]]
           buttons = [[3], [1, 3], [2, 3], [0, 2]]
-      solve pressedButtons initialState desiredState buttons `shouldBe` [[[0, 2], [0, 1]]]
+      solve pressedButtons not buttonsAfter initialState desiredState buttons `shouldBe` [[[0, 2], [0, 1]]]
 
   describe "start" $ do
     it "starts the first machine" $ do
       let desiredState = [False, True, True, False]
           buttons = [[3], [1, 3], [2, 3], [0, 2], [0, 1]]
-          machine = Machine desiredState buttons
-      start machine `shouldBe` Just [[0, 2], [0, 1]]
+          machine = (desiredState, buttons)
+      start machine not buttonsAfter False `shouldBe` Just [[0, 2], [0, 1]]
     it "starts the second machine" $ do
       let desiredState = [False, False, False, True, False]
           buttons = [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]]
-          machine = Machine desiredState buttons
-      start machine `shouldBe` Just [[1, 2, 3, 4], [0, 4], [0, 1, 2]]
+          machine = (desiredState, buttons)
+      start machine not buttonsAfter False `shouldBe` Just [[1, 2, 3, 4], [0, 4], [0, 1, 2]]
     it "starts the third machine" $ do
       let desiredState = [False, True, True, True, False, True]
           buttons = [[0, 1, 2, 3, 4], [0, 3, 4], [0, 1, 2, 4, 5], [1, 2]]
-          machine = Machine desiredState buttons
-      start machine `shouldBe` Just [[0, 3, 4], [0, 1, 2, 4, 5]]
+          machine = (desiredState, buttons)
+      start machine not buttonsAfter False `shouldBe` Just [[0, 3, 4], [0, 1, 2, 4, 5]]
+
+  describe "checkState" $ do
+    it "filters buttons that do not exceed desired state" $ do
+      let desiredState = [2, 1]
+          state = [2, 0]
+          buttons = [[0], [1]]
+          result = checkState [] state desiredState buttons
+      result `shouldBe` [[1]]
 
   describe "puzzle1" $ do
     it "sums all results" $ do
       let input =
-            [ Machine [False, True, True, False] [[3], [1, 3], [2, 3], [0, 2], [0, 1]]
-            , Machine [False, False, False, True, False] [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]]
-            , Machine [False, True, True, True, False, True] [[0, 1, 2, 3, 4], [0, 3, 4], [0, 1, 2, 4, 5], [1, 2]]
+            [ ([False, True, True, False], [[3], [1, 3], [2, 3], [0, 2], [0, 1]])
+            , ([False, False, False, True, False], [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]])
+            , ([False, True, True, True, False, True], [[0, 1, 2, 3, 4], [0, 3, 4], [0, 1, 2, 4, 5], [1, 2]])
             ]
       puzzle1 input `shouldReturn` 7
 
     it "processes input file" $ do
       contents <- readFile "input"
-      let parseMachine :: String -> Machine
+      let parseMachine :: String -> ([Bool], [Button])
           parseMachine str =
             case words str of
-              [] -> Machine [] []
+              [] -> ([], [])
               (indicatorsStr : buttonsStr) ->
                 let indicators = map (== '#') (init $ tail indicatorsStr)
                     buttons = map (map read . splitOn [','] . tail . init) (init buttonsStr)
-                 in Machine indicators buttons
+                 in (indicators, buttons)
           machines = map parseMachine $ lines contents
-      print machines
       result <- puzzle1 machines
       pendingWith (show result)
 
-  describe "puzzle2" $ do
+  focus $ describe "puzzle2" $ do
     it "solves the puzzle for given input" $ do
-      pending
+      let input =
+            [ ([3, 5, 4, 7], [[3], [1, 3], [2, 3], [0, 2], [0, 1]])
+            , ([7, 5, 12, 7, 2], [[0, 2, 3, 4], [2, 3], [0, 4], [0, 1, 2], [1, 2, 3, 4]])
+            , ([10, 11, 11, 5, 10, 5], [[0, 1, 2, 3, 4], [0, 3, 4], [0, 1, 2, 4, 5], [1, 2]])
+            ]
+      puzzle2 input `shouldReturn` 33
 
     it "processes input file" $ do
-      pending
+      contents <- readFile "input"
+      let parseMachine :: String -> ([Int], [Button])
+          parseMachine str =
+            case words str of
+              [] -> ([], [])
+              (_ : buttonsStr) ->
+                let joltages = map read (splitOn [','] $ tail $ init $ last buttonsStr)
+                    buttons = map (map read . splitOn [','] . tail . init) (init buttonsStr)
+                 in (joltages, buttons)
+          machines = map parseMachine $ lines contents
+      result <- puzzle2 machines
+      pendingWith (show result)
