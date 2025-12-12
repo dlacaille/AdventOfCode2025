0c8557569bc36b8f2c1e238f7fd1a0f10def760d
Dominic Lacaille <dominic.lacaille@acuityinc.com>
2025-12-12 08:52:58 -0500

    Day10 pt 2

--- day10/Solution.hs
+++ day10/Solution.hs
@@ -1,15 +1,13 @@
 {-# LANGUAGE BangPatterns #-}
+
 module Solution where
 
 import Control.Monad (msum)
-import Control.Parallel.Strategies (parListChunk, rdeepseq, using)
-import Data.List (sortOn, minimumBy, subsequences)
+import Data.List (sortOn)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Maybe (listToMaybe, mapMaybe)
-import Debug.Trace (trace)
-import Data.Ord (Down(Down), comparing)
-import Data.Time.Clock (getCurrentTime, diffUTCTime)
-import System.IO.Unsafe (unsafePerformIO)
-import Text.Printf (printf)
+import Data.Ord (Down (Down))
 
 type Button = [Int]
 type State a = [a]
@@ -18,7 +16,6 @@ type NextButton a = Button -> [a] -> [a] -> [Button] -> [Button]
 buttonsAfter :: NextButton Bool
 buttonsAfter button _ _ = filter (> button)
 
--- Solve for puzzle1 (Bool) using backtracking search
 solve1 :: [Button] -> [Bool] -> [Button] -> [[Button]]
 solve1 pressedButtons desiredState buttons
   | not (null solutions) = [button : pressedButtons | (_, button) <- take 1 solutions]
@@ -30,72 +27,62 @@ solve1 pressedButtons desiredState buttons
         | (_, button) <- nextStates
         ]
  where
-  currentState = foldl (\s btn -> zipWith (\i val -> if i `elem` btn then not val else val) [0..] s) (replicate (length desiredState) False) pressedButtons
+  currentState = foldl (\s btn -> zipWith (\i val -> if i `elem` btn then not val else val) [0 ..] s) (replicate (length desiredState) False) pressedButtons
   nextStates = [(zipWith (\i s -> if i `elem` btn then not s else s) [0 ..] currentState, btn) | btn <- buttons]
   solutions = sortOn (length . snd) [(s, b) | (s, b) <- nextStates, s == desiredState]
 
+patterns :: [[Int]] -> Map [Int] Int
+patterns coeffs = Map.fromListWith min allPatterns
+ where
+  numButtons = length coeffs
+  numVariables = if null coeffs then 0 else length (head coeffs)
+  zeroVector = replicate numVariables 0
+  allPatterns =
+    [ (pattern, patternLen)
+    | patternLen <- [0 .. numButtons]
+    , buttons <- subsequencesOfSize patternLen [0 .. numButtons - 1]
+    , let pattern = foldl (zipWith (+)) zeroVector [coeffs !! i | i <- buttons]
+    ]
+  subsequencesOfSize 0 _ = [[]]
+  subsequencesOfSize _ [] = []
+  subsequencesOfSize n (x : xs) = map (x :) (subsequencesOfSize (n - 1) xs) ++ subsequencesOfSize n xs
+
 solve2 :: [Button] -> [Int] -> [Button] -> Maybe [Button]
-solve2 _ desiredState buttons
-  | all (== 0) desiredState = Just []
-  | otherwise = minimumByMaybe (comparing length) . mapMaybe trySolve $ subsequences (zip buttons buttonVectors)
-  where
-    !n = length desiredState
-    !buttonVectors = map toVector buttons
-    toVector btn = [if i `elem` btn then 1 else 0 | i <- [0 .. n - 1]]
+solve2 _ desiredState buttons =
+  let !coeffs = map toVector buttons
+      !n = length desiredState
+      !patternCosts = patterns coeffs
+      toVector btn = [if i `elem` btn then 1 else 0 | i <- [0 .. n - 1]]
 
-    minimumByMaybe _ [] = Nothing
-    minimumByMaybe cmp xs = Just (minimumBy cmp xs)
+      solveMemo :: Map [Int] Int -> [Int] -> (Int, Map [Int] Int)
+      solveMemo memo goal
+        | all (== 0) goal = (0, memo)
+        | Just cached <- Map.lookup goal memo = (cached, memo)
+        | otherwise =
+            let (answer, finalMemo) = foldl tryPattern (1000000, memo) (Map.toList patternCosts)
+             in (answer, Map.insert goal answer finalMemo)
+       where
+        tryPattern (bestSoFar, currentMemo) (pattern, patternCost)
+          | all (\(p, g) -> p <= g && p `mod` 2 == g `mod` 2) (zip pattern goal) =
+              let newGoal = zipWith (\g p -> (g - p) `div` 2) goal pattern
+                  (subResult, newMemo) = solveMemo currentMemo newGoal
+                  candidate = patternCost + 2 * subResult
+               in (min bestSoFar candidate, newMemo)
+          | otherwise = (bestSoFar, currentMemo)
 
-    trySolve [] = if all (== 0) desiredState then Just [] else Nothing
-    trySolve combo = listToMaybe $ tryAllCounts (unzip combo) desiredState
-
-    tryAllCounts ([], []) target
-      | all (== 0) target = [[]]
-      | otherwise = []
-    tryAllCounts (btn : btns, vec : vecs) target =
-      [ replicate count btn ++ rest
-      | count <- [0 .. maxCount vec target]
-      , let !newTarget = zipWith (-) target (map (* count) vec)
-      , all (>= 0) newTarget
-      , rest <- tryAllCounts (btns, vecs) newTarget
-      ]
-    tryAllCounts _ _ = []
-
-    maxCount vec target = 
-      let !result = minimum [if v > 0 then target !! i `div` v else maxBound | (i, v) <- zip [0 ..] vec, v > 0]
-      in result
-
-start1 :: ([Bool], [Button]) -> Maybe [Button]
-start1 (desiredState, buttons) =
-  listToMaybe $ sortOn length $ solve1 [] desiredState sortedButtons
- where
-  sortedButtons = sortOn (Down . length) buttons
-
-start2 :: ([Int], [Button]) -> Maybe [Button]
-start2 (desiredState, buttons) =
-  solve2 [] desiredState sortedButtons
- where
-  sortedButtons = sortOn (Down . length) buttons
-
-timedSolve :: Int -> Int -> (a -> b) -> a -> b
-timedSolve idx total operation input = unsafePerformIO $ do
-  startTime <- getCurrentTime
-  let !result = operation input
-  endTime <- getCurrentTime
-  let elapsed = diffUTCTime endTime startTime
-  let msg = printf "Solved Machine %d/%d (%.2fs)" idx total (realToFrac elapsed :: Double)
-  return $ trace msg result
+      (result, _) = solveMemo Map.empty desiredState
+   in if result >= 1000000 then Nothing else Just (replicate result [0])
 
 puzzle1 :: [([Bool], [Button])] -> IO Int
-puzzle1 machines = return . sum . map length $ results
-  where total = length machines
-        indexedMachines = zip [1 :: Int ..] machines
-        results = mapMaybe (\(idx, m) -> timedSolve idx total start1 m) indexedMachines
+puzzle1 machines = return . sum . map length $ mapMaybe start machines
+ where
+  start (desiredState, buttons) =
+    let sortedButtons = sortOn (Down . length) buttons
+     in listToMaybe $ sortOn length $ solve1 [] desiredState sortedButtons
 
 puzzle2 :: [([Int], [Button])] -> IO Int
-puzzle2 machines = return . sum . map length $ results
-  where
-    total = length machines
-    sortedMachines = sortOn (length . fst) machines
-    indexedMachines = zip [1 :: Int ..] sortedMachines
-    results = mapMaybe (\(idx, m) -> timedSolve idx total start2 m) indexedMachines `using` parListChunk 4 rdeepseq
+puzzle2 machines = return . sum . map length $ mapMaybe start machines
+ where
+  start (desiredState, buttons) =
+    let sortedButtons = sortOn (Down . length) buttons
+     in solve2 [] desiredState sortedButtons
--- day10/test/Spec.hs
+++ day10/test/Spec.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List.Split (splitOn)
 import Solution
 import Test.Hspec
 
@@ -54,6 +55,21 @@ main = hspec $ do
             ]
       puzzle1 input `shouldReturn` 7
 
+    it "processes input file" $ do
+      contents <- readFile "input"
+      let parseMachine :: String -> ([Bool], [[Int]])
+          parseMachine str =
+            case words str of
+              [] -> ([], [])
+              (indicatorsStr : buttonsStr) ->
+                let indicators = map (== '#') (init $ tail indicatorsStr)
+                    buttons = map (map read . splitOn [','] . tail . init) (init buttonsStr)
+                 in (indicators, buttons)
+          machines = map parseMachine $ lines contents
+      print machines
+      result <- puzzle1 machines
+      pendingWith $ show result
+
   describe "puzzle2" $ do
     it "solves the puzzle for given input" $ do
       let input =
@@ -62,3 +78,27 @@ main = hspec $ do
             , ([10, 11, 11, 5, 10, 5], [[0, 1, 2, 3, 4], [0, 3, 4], [0, 1, 2, 4, 5], [1, 2]])
             ]
       puzzle2 input `shouldReturn` 33
+
+    it "solves a slow machine" $ do
+      let input =
+            [
+              ( [36, 74, 63, 66, 60, 64, 66, 59, 67, 31]
+              , [[3, 5, 6, 7], [0, 3, 4, 5, 6, 8, 9], [1, 3, 8], [0, 2, 3, 4, 5, 6, 7, 8, 9], [0, 1, 3, 4, 5, 6, 8], [1, 7], [0, 1, 2, 4, 5, 7, 9], [1, 2, 3, 4, 6, 7, 8, 9], [1, 2, 4, 5], [2, 6, 8], [0, 1, 2, 3, 4, 5, 7, 9], [0, 7], [1, 3, 5, 8]]
+              )
+            ]
+      result <- puzzle2 input
+      pendingWith $ show result
+
+    it "processes input file" $ do
+      contents <- readFile "input"
+      let parseMachine :: String -> ([Int], [[Int]])
+          parseMachine str =
+            case words str of
+              [] -> ([], [])
+              (_ : buttonsStr) ->
+                let joltages = map read (splitOn [','] $ tail $ init $ last buttonsStr)
+                    buttons = map (map read . splitOn [','] . tail . init) (init buttonsStr)
+                 in (joltages, buttons)
+          machines = map parseMachine $ lines contents
+      result <- puzzle2 machines
+      pendingWith $ show result
